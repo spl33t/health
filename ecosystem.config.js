1d675d1b495c07a2c666c8e7a03c97617b29f20b
Maxim Kiselev <spl33t@ya.ru>
2026-03-18 19:02:34 +0300

    init

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,9 +4,11 @@ module.exports = {
             name: 'health-monitor',
             script: 'dist/index.js',
             instances: 1,
+            exec_mode: 'fork',
             autorestart: true,
             watch: false,
-            max_memory_restart: '300M',
+            max_memory_restart: '512M',
+            cwd: __dirname,
             env: {
                 NODE_ENV: 'production',
             },
